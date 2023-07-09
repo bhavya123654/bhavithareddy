e0551c48198e93db49006607827c683918275b0b
bvina <bvinaydevops@gmail.com>
2023-07-09 00:32:04 -0400

    new update

---
+++ MVC Agile Process with Agile Work Item Process/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVC_Agile_Process_with_Agile_Work_Item_Process.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
